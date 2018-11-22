03953ce0be439227ebda81652a8e185488bd880f
prithaupadhyay <prithaupadhyay@gmail.com>
2018-11-22 23:32:37 +0530

    Added salt and pepper noise with different density code

---
+++ saltAndPepper_noiseDensity.m
@@ -0,0 +1,49 @@
+#add different amount of salt & pepper noise and remove it
+pkg load image;
+clear;
+clc;
+clf;
+
+p=imread('cameraman.tif');
+
+#d=noise density
+d=0.02;
+j = imnoise(p,'salt & pepper',d);
+subplot(1,4,1)
+imshow(j)
+
+d=0.08;
+k = imnoise(p,'salt & pepper',d);
+subplot(1,4,2)
+imshow(k)
+
+d=0.1;
+l = imnoise(p,'salt & pepper',d);
+subplot(1,4,3)
+imshow(l)
+
+d=0.5;
+m = imnoise(p,'salt & pepper',d);
+subplot(1,4,4)
+imshow(m)
+
+#################applying median filter##############
+figure
+
+x=medfilt2(j);
+subplot(1,4,1)
+imshow(x)
+
+x=medfilt2(k);
+subplot(1,4,2)
+imshow(x)
+
+
+x=medfilt2(l);
+subplot(1,4,3)
+imshow(x)
+
+
+x=medfilt2(m);
+subplot(1,4,4)
+imshow(x)
